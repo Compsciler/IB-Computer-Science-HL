9cbc3476242cfbe369a45740b5f60ac39bd6981d
Roger Wang <rogerwang2520@gmail.com>
2020-10-12 09:10:57 -0700

    Updated Levenshtein Distance

--- LevenshteinDistance/src/Main.java
+++ LevenshteinDistance/src/Main.java
@@ -1,15 +1,14 @@
 import java.io.*;
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class Main {
     public static Set<String> dictionaryWords;
     public static Map<String, Set<String>> dictionaryMap;
 
-    public static String startWord = "";  // Must be in dictionary
-    public static String endWord = "";  // Must be in dictionary
-    public static Queue<String> wordsToCheck;
-    public static Set<String> checkedWords;
+    public static String startWord = "abbas";  // Must be in dictionary
+    public static String endWord = "abbes";  // Must be in dictionary
+    public static Queue<String> wordsToVisit;
+    public static Set<String> locatedWords;
     public static Map<String, String> parentWordMap;
 
     public static void main(String[] args) throws FileNotFoundException {
@@ -19,7 +18,7 @@ public class Main {
         dictionaryWords = new HashSet<>();
         dictionaryMap = new HashMap<>();
 
-        loadDictionary("dictionary words smaller.txt");
+        loadDictionary("dictionary words small.txt");
         // System.out.println(dictionaryMap);
 
         pickRandomStartAndEndWordIfEmpty();
@@ -32,16 +31,16 @@ public class Main {
         System.out.println("Execution time: " + (endTimeInMillis - startTimeInMillis) * 1000 + " s");
     }
 
-    public static List<String> calculateLevDistance(String word1, String word2){
-        wordsToCheck = new PriorityQueue<>();
-        checkedWords = new HashSet<>();
+    public static List<String> calculateLevDistance(String word1, String word2) {
+        wordsToVisit = new PriorityQueue<>();
+        locatedWords = new HashSet<>();
         parentWordMap = new HashMap<>();  // Key word's parent is value word
         boolean isWord2Found = false;
 
         addAdjacentWords(word1);
-        while (!wordsToCheck.isEmpty()){
-            String currentWord = wordsToCheck.poll();
-            if (currentWord.equals(word2)){
+        while (!wordsToVisit.isEmpty()) {
+            String currentWord = wordsToVisit.poll();
+            if (currentWord.equals(word2)) {
                 isWord2Found = true;
                 break;
             }
@@ -49,9 +48,9 @@ public class Main {
         }
 
         LinkedList<String> res = new LinkedList<>();
-        if (isWord2Found){
+        if (isWord2Found) {
             res.addFirst(word2);
-            while (!res.getFirst().equals(word1)){
+            while (!res.getFirst().equals(word1)) {
                 String parentWord = parentWordMap.get(res.getFirst());
                 res.addFirst(parentWord);
             }
@@ -59,16 +58,105 @@ public class Main {
         return res;
     }
 
-    public static void addAdjacentWords(String word){
+    public static void addAdjacentWords(String word) {
         Set<String> adjacentWords = dictionaryMap.get(word);
-        if (adjacentWords != null){
-            adjacentWords.removeAll(checkedWords);
-            for (String adjacentWord: adjacentWords){
-                wordsToCheck.add(adjacentWord);
+        if (adjacentWords != null) {
+            adjacentWords.removeAll(locatedWords);
+            for (String adjacentWord : adjacentWords) {
+                wordsToVisit.add(adjacentWord);
                 parentWordMap.put(adjacentWord, word);
+                locatedWords.add(adjacentWord);  // Moved inside loop to not create long distance of [fig, big, dig, dog]
             }
         }
-        checkedWords.add(word);
+    }
+
+    public static void loadDictionary(String filePath) throws FileNotFoundException {
+        File file = new File(filePath);
+        Scanner sc = new Scanner(file);
+
+        // Reduces n^2 searches to n(n-1)/2
+        while (sc.hasNextLine()) {
+            String newWord = sc.nextLine();
+            for (String dictionaryWord : dictionaryWords) {
+                if (areWordsLevAdjacent(newWord, dictionaryWord)) {
+                    updateMap(newWord, dictionaryWord);
+                }
+            }
+            dictionaryWords.add(newWord);
+        }
+    }
+
+    public static boolean areWordsLevAdjacent(String word1, String word2) {
+        if (word1.length() != word2.length()) {
+            return false;
+        }
+
+        int differentLetterTotal = 0;
+        for (int i = 0; i < word1.length(); i++) {
+            if (word1.charAt(i) != word2.charAt(i)) {
+                differentLetterTotal++;
+                if (differentLetterTotal >= 2) {
+                    return false;
+                }
+            }
+        }
+        return (differentLetterTotal != 0);
+    }
+
+    public static void updateMap(String word1, String word2) {
+        String[][] wordUpdates = {{word1, word2}, {word2, word1}};
+        for (String[] wordUpdate : wordUpdates) {
+            String wordKey = wordUpdate[0];
+            String wordValue = wordUpdate[1];
+
+            if (dictionaryMap.containsKey(wordKey)) {
+                dictionaryMap.get(wordKey).add(wordValue);
+            } else {
+                Set<String> wordSet = new HashSet<>();
+                wordSet.add(wordValue);
+                dictionaryMap.put(wordKey, wordSet);
+            }
+        }
+    }
+
+    public static List<String> pickRandomWordsFromDictionary(int numWords) {
+        Random rand = new Random();
+        TreeSet<Integer> randomIndices = new TreeSet<>();
+        int dictionarySize = dictionaryWords.size();
+        for (int i = 0; i < numWords; i++) {
+            randomIndices.add(rand.nextInt(dictionarySize));
+        }
+        List<String> res = new ArrayList<>();
+        int i = 0;
+        for (String s : dictionaryWords) {
+            if (i == randomIndices.first()) {
+                res.add(s);
+                randomIndices.pollFirst();
+                if (randomIndices.isEmpty()) {
+                    break;
+                }
+            }
+            i++;
+        }
+        return res;
+    }
+
+    public static void pickRandomStartAndEndWordIfEmpty() {
+        int randomWordTotal = 0;
+        randomWordTotal += (startWord.equals("")) ? 1 : 0;
+        randomWordTotal += (endWord.equals("")) ? 1 : 0;
+
+        if (randomWordTotal >= 1) {
+            List<String> randomWords = pickRandomWordsFromDictionary(randomWordTotal);
+            if (startWord.equals("")) {
+                startWord = randomWords.get(0);
+                randomWords.remove(0);
+            }
+            if (endWord.equals("")) {
+                endWord = randomWords.get(0);
+                randomWords.remove(0);
+            }
+        }
     }
 
     /*
@@ -90,96 +178,4 @@ public class Main {
         checkedWords.add(startWord);
     }
     */
-
-    public static void loadDictionary(String filePath) throws FileNotFoundException {
-        File file = new File(filePath);
-        Scanner sc = new Scanner(file);
-
-        // Reduces n^2 searches to n(n-1)/2
-        while (sc.hasNextLine()){
-            String newWord = sc.nextLine();
-            for (String dictionaryWord: dictionaryWords){
-                if (areWordsLevAdjacent(newWord, dictionaryWord)){
-                    updateMap(newWord, dictionaryWord);
-                }
-            }
-            dictionaryWords.add(newWord);
-        }
-    }
-
-    public static boolean areWordsLevAdjacent(String word1, String word2){
-        if (word1.length() != word2.length()){
-            return false;
-        }
-
-        int differentLetterTotal = 0;
-        for (int i = 0; i < word1.length(); i++){
-            if (word1.charAt(i) != word2.charAt(i)){
-                differentLetterTotal++;
-                if (differentLetterTotal >= 2){
-                    return false;
-                }
-            }
-        }
-        if (differentLetterTotal == 0){
-            return false;
-        }
-        return true;
-    }
-
-    public static void updateMap(String word1, String word2){
-        String[][] wordUpdates = {{word1, word2}, {word2, word1}};
-        for (String[] wordUpdate: wordUpdates){
-            String wordKey = wordUpdate[0];
-            String wordValue = wordUpdate[1];
-
-            if (dictionaryMap.containsKey(wordKey)){
-                dictionaryMap.get(wordKey).add(wordValue);
-            } else {
-                Set<String> wordSet = new HashSet<>();
-                wordSet.add(wordValue);
-                dictionaryMap.put(wordKey, wordSet);
-            }
-        }
-    }
-
-    public static List<String> pickRandomWordsFromDictionary(int numWords){
-        Random rand = new Random();
-        TreeSet<Integer> randomIndices = new TreeSet<>();
-        int dictionarySize = dictionaryWords.size();
-        for (int i = 0; i < numWords; i++){
-            randomIndices.add(rand.nextInt(dictionarySize));
-        }
-        List<String> res = new ArrayList<>();
-        int i = 0;
-        for (String s: dictionaryWords){
-            if (i == randomIndices.first()){
-                res.add(s);
-                randomIndices.pollFirst();
-                if (randomIndices.isEmpty()){
-                    break;
-                }
-            }
-            i++;
-        }
-        return res;
-    }
-
-    public static void pickRandomStartAndEndWordIfEmpty(){
-        int randomWordTotal = 0;
-        randomWordTotal += (startWord.equals("")) ? 1 : 0;
-        randomWordTotal += (endWord.equals("")) ? 1 : 0;
-
-        if (randomWordTotal >= 1){
-            List<String> randomWords = pickRandomWordsFromDictionary(randomWordTotal);
-            if (startWord.equals("")){
-                startWord = randomWords.get(0);
-                randomWords.remove(0);
-            }
-            if (endWord.equals("")){
-                endWord = randomWords.get(0);
-                randomWords.remove(0);
-            }
-        }
-    }
 }
