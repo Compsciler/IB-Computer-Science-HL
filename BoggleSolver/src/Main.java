a5b9cb6ec6f66e86428d95bb567dca5d2ff50de4
Roger Wang <rogerwang2520@gmail.com>
2020-11-04 21:41:23 -0800

    Updated Boggle Solver

--- BoggleSolver/src/Main.java
+++ BoggleSolver/src/Main.java
@@ -4,25 +4,47 @@ import java.util.*;
 public class Main {
     public static Set<String> dictionaryWords;
     public static char[][] grid =   {
+                                        {'P', 'E', 'R', 'S'},
+                                        {'L', 'A', 'T', 'G'},
+                                        {'S', 'I', 'N', 'E'},
+                                        {'T', 'E', 'R', 'S'}
+                                    };
+    /*
+    public static char[][] grid =   {
+                                        {'S', 'E', 'T', 'S'},
+                                        {'P', 'I', 'R', 'D'},
+                                        {'L', 'A', 'N', 'E'},
+                                        {'S', 'E', 'T', 'S'}
+                                    };
+    */
+    /*
+    public static char[][] grid =   {
                                         {'D', 'R', 'C', 'K'},
                                         {'A', 'E', 'W', 'N'},
                                         {'I', 'T', 'S', 'U'},
                                         {'O', 'N', 'G', 'E'}
                                     };
+    */
 
     public static void main(String[] args) throws FileNotFoundException {
-        dictionaryWords = new HashSet<>();
-        loadDictionary("dictionary words small.txt");
-        Set<String> validWordSet = solve();
-        List<ValidWord> validWordsArr = new ArrayList<>();
+        long startTimeInMillis = System.currentTimeMillis();
+
+        dictionaryWords = new HashSet<>();  // DEMO 1
+        loadDictionary("dictionary words small.txt");  // DEMO 2
+        Set<String> validWordSet = solve();  // DEMO 3
+        List<ValidWord> validWordsArr = new ArrayList<>();  // DEMO 7
         for (String word: validWordSet){
             validWordsArr.add(new ValidWord(word));
             // System.out.println(word);
         }
-        Collections.sort(validWordsArr);
+        Collections.sort(validWordsArr);  // DEMO 9
         for (ValidWord validWordObj: validWordsArr){
             System.out.println(validWordObj);
         }
+        System.out.println("Total: " + validWordsArr.size());
+
+        long endTimeInMillis = System.currentTimeMillis();
+        System.out.println("Execution time: " + (endTimeInMillis - startTimeInMillis) / 1000.0 + " s");
     }
 
     public static void loadDictionary(String filePath) throws FileNotFoundException {
@@ -38,7 +60,7 @@ public class Main {
         int gridIndex = 0;
         for (int r = 0; r < grid.length; r++){
             for (int c = 0; c < grid.length; c++){
-                validWords.addAll(solve(gridIndex, "", cloneGrid(grid)));
+                validWords.addAll(solve(gridIndex, "", cloneGrid(grid)));  // DEMO 4
                 gridIndex++;
             }
         }
@@ -56,7 +78,7 @@ public class Main {
         }
 
         // The base case is when this loop only reaches continue statements
-        for (int i = -1; i <= 1; i++){
+        for (int i = -1; i <= 1; i++){  // DEMO 5
             int row = newLetterPos[0] + i;
             if (row < 0 || row >= grid.length){
                 continue;
@@ -69,7 +91,7 @@ public class Main {
                 if (remainingGrid[row][col] == ' '){
                     continue;
                 }
-                validWords.addAll(solve(getGridIndex(row, col), wordSoFar, cloneGrid(remainingGrid)));
+                validWords.addAll(solve(getGridIndex(row, col), wordSoFar, cloneGrid(remainingGrid)));  // DEMO 6
             }
         }
         return validWords;
@@ -106,7 +128,7 @@ class ValidWord implements Comparable<ValidWord> {
     }
 
     @Override
-    public int compareTo(ValidWord other){
+    public int compareTo(ValidWord other){  // DEMO 8
         if (word.length() != other.word.length()){
             return other.word.length() - word.length();
         }
